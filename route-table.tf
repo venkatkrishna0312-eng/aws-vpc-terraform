70f0dfba15816142ce8a7d9398fbe61f0cc9fc6b
Venkat Krishna <venkatkrishna0312@gmail.com>
2026-07-26 16:09:00 +0530

    Update Terraform format check step--local files formatted using fmt

--- route-table.tf
+++ route-table.tf
@@ -1,44 +1,44 @@
 resource "aws_route_table" "public_rt" {
 
 
-    vpc_id = aws_vpc.main.id
-    tags = {
-        Name = "${var.vpc_name}-public-rt"
-    }
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "${var.vpc_name}-public-rt"
+  }
 }
 
 resource "aws_route" "internet_access" {
-    route_table_id         = aws_route_table.public_rt.id
-    destination_cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main_igw.id
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main_igw.id
 
 }
 
 resource "aws_route_table_association" "public_rt_assoc" {
-    subnet_id      = aws_subnet.public_subnet.id
-    route_table_id = aws_route_table.public_rt.id
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
 
-  
+
 }
 
 resource "aws_route_table" "private_rt" {
-    vpc_id = aws_vpc.main.id
-    tags = {
-        Name = "${var.vpc_name}-private-rt"
-    }
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "${var.vpc_name}-private-rt"
+  }
 }
 
 resource "aws_route" "private_intenet_access" {
 
-    route_table_id         = aws_route_table.private_rt.id
-    destination_cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.main_nat_gateway.id
+  route_table_id         = aws_route_table.private_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.main_nat_gateway.id
 
 }
 
 resource "aws_route_table_association" "private_rt_assoc" {
-    subnet_id      = aws_subnet.private_subnet.id
-    route_table_id = aws_route_table.private_rt.id
-  
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_rt.id
+
 }
 
